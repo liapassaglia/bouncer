eb9ed28b8fb23f313c7920b9ce33cd0f86bef0d6
liapassaglia <liapassaglia@gmail.com>
2022-01-13 14:19:57 -0500

    Init React Native project and React Navigation Setup

---
+++ GoerApp/Screens/ExploreScr.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function ExploreScr() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Explore Screen</Text>
+      </View>
+    );
+}
+
+export default ExploreScr;
